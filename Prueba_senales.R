80000bb9a6dc7a3dcf5c0e42de60cd1349bda434
nacho <ignaciolebrero@gmail.com>
2019-10-03 18:31:34 -0400

    prueba borrado

--- Prueba_senales.R
+++ Prueba_senales.R
@@ -1,4 +1,5 @@
 library(tuneR)
+library(seewave)
 
 plotTimeAndFrecuencyDomains <- function(directory, samples)
 {
@@ -14,12 +15,28 @@ plotTimeAndFrecuencyDomains <- function(directory, samples)
 	timeArray <- (0:(length(s1)-1)) / sndObj@samp.rate
 	timeArray <- timeArray * samples #scale to milliseconds
 
+	######
+
+	fft.s1 = fft(s1)
+
+	filtroDF = rep(1, samples)
+	# filtroDF [1500:(samples-500)] = 0
+	filtroDF [500:(samples-500)] = 0
+
+	fft.s1 = filtroDF * fft.s1 
+
+	#####
 	# Ploteo el dominio del tiempo
-	plot(timeArray, s1, type='l', col='black', xlab='Time (ms)', ylab='Amplitude')
+	retras = Re(fft(fft.s1, inverse=TRUE) / samples)
+	plot(timeArray, retras, type='l', col='black', xlab='Time (ms)', ylab='Amplitude')
 
 	# Ploteo el dominio de la frecuencia
-	fft.s1 = fft(s1)
+	# fft.s1 = fft(s1)
 	plot(Mod(fft.s1), type='l')
+
+	# Save result
+	saveWav(retras, f=22050)
 }
+
 
 plotTimeAndFrecuencyDomains('audios/Guitarra/Philarmonica/guitar_A2_very-long_forte_normal.mp3', 90000)
